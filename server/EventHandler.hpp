93b836451fc4b172d08464d5a1d6f875a07267f9
ongonginging <ongonginging@gmail.com>
2015-08-21 13:17:24 +0800

    backup

---
+++ server/EventHandler.hpp
@@ -0,0 +1,15 @@
+
+#ifndef __EVENTHANDLER_HPP__
+#define __EVENTHANDLER_HPP__
+
+#include <event.h>
+
+class EventHandler{
+private:
+    struct event_base* base;
+public:
+
+};
+
+#endif //__EVENTHANDLER_HPP__
+
