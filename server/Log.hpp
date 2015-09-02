ce06023b3ba1eb1071d6797fd435737e03330611
ongonginging <ongonginging@gmail.com>
2015-09-03 01:16:52 +0800

    backup

--- server/Log.hpp
+++ server/Log.hpp
@@ -2,6 +2,8 @@
 #ifndef __LOG_HPP__
 #define __LOG_HPP__
 
+#include <iostream>
+#include <sstream>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 #define LOG_ENTER_FUNC(a) { \
@@ -19,5 +21,38 @@
 #define GLOBAL_LOG_LEAVE_FUNC(a)  { \
         std::cout<<boost::posix_time::microsec_clock::universal_time()<<" "<<__func__<<" leave "<<a<<std::endl; \
 }
+
+class Logger{
+    private:
+        std::vector<std::string> vecOutput;
+        std::string funcName;
+        std::string fileName;
+        int lineNum;
+    public:
+        Logger(const std::string& fileName, const std::string& funcName, const int& lineNum){
+            this->fileName = fileName;
+            this->funcName = funcName;
+            this->lineNum = lineNum;
+        }
+        template<typename T>
+        void info(T v){
+            this->vecOutput.push_back(boost::lexical_cast<std::string>(v));
+            std::string tmp;
+            for(auto s: this->vecOutput){
+                tmp = tmp + s + " ";
+            }
+            std::cout<<boost::posix_time::microsec_clock::universal_time()<<" "<<this->fileName<<" "<<this->funcName<<" "<<this->lineNum<<" "<<tmp<<" "<<std::endl;
+        }
+        template<typename T1, typename... T2>
+        void info(T1 v1, T2... v2){
+            this->vecOutput.push_back(boost::lexical_cast<std::string>(v1));
+            info(v2...);
+        }
+        ~Logger(){
+        }
+};
+
+#define LOGGER() Logger(__FILE__, __func__, __LINE__)
+
 
 #endif //__LOG_HPP__
