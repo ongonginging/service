61b114b00839d2df6cf0fa79c86c0d36fc371f12
ongonginging <ongonginging@gmail.com>
2015-09-02 21:36:52 +0800

    backup

--- client/main.cpp
+++ client/main.cpp
@@ -14,6 +14,8 @@
 
 #include<event.h>
 
+#include <boost/date_time/posix_time/posix_time.hpp> 
+
 static unsigned char readBuffer[65535];
 static unsigned char writeBuffer[65535];
 
@@ -24,12 +26,14 @@ void cb(evutil_socket_t fd, short event, void* arg){
 int main(int arc, char* argv[]){
     int rv = 0;
     int fd = ::socket(AF_INET, SOCK_STREAM, 0);
+    std::cout<<boost::posix_time::microsec_clock::universal_time()<<std::endl;
     if(fd == -1){
         std::cout<<__func__<<" created client socket failed. errno = "<<errno<<std::endl;
         return -1;
     }else{
         std::cout<<__func__<<" successful created client socket "<<fd<<std::endl;
     }
+    
     /*
     int result = fcntl(fd, F_SETFL, O_NONBLOCK|fcntl(fd, F_GETFL));
     if (result == -1){
@@ -41,25 +45,18 @@ int main(int arc, char* argv[]){
     memset(&addr, 0, sizeof(struct sockaddr_in));
     addr.sin_family = AF_INET;                 
     addr.sin_port = htons(9544);          
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    addr.sin_addr.s_addr = inet_addr("0.0.0.0"); 
+    std::cout<<boost::posix_time::microsec_clock::universal_time()<<std::endl;
     int ret = connect(fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
     if (ret == -1){
         std::cout<<__func__<<" connect server failed. errno = "<<errno<<std::endl;
         rv = -1;
     }
+    std::cout<<boost::posix_time::microsec_clock::universal_time()<<std::endl;
     ret = read(fd, readBuffer, sizeof(readBuffer));
     if (ret == -1){
         std::cout<<__func__<<" read -1. errno = "<<errno<<std::endl;
     }
     close(fd);
-    //struct event_base* base = event_base_new();
-    //struct event* ev;
-    //ev = event_new(base, fd, EV_READ|EV_PERSIST, cb, NULL);
-    //event_add(ev, NULL);
-    //event_base_dispatch(base);
-    //event_free(ev);
-    //event_base_free(base);
-    //base = NULL;
-    //ev = NULL;
     return rv;
 }
--- server/Log.hpp
+++ server/Log.hpp
@@ -1,17 +1,23 @@
 
+#ifndef __LOG_HPP__
+#define __LOG_HPP__
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #define LOG_ENTER_FUNC(a) { \
-        std::cout<<this->className<<"::"<<__func__<<" enter "<<a<<std::endl; \
+        std::cout<<boost::posix_time::microsec_clock::universal_time()<<" "<<this->className<<"::"<<__func__<<" enter "<<a<<std::endl; \
 }
 
 #define LOG_LEAVE_FUNC(a)  { \
-        std::cout<<this->className<<"::"<<__func__<<" leave "<<a<<std::endl; \
+        std::cout<<boost::posix_time::microsec_clock::universal_time()<<" "<<this->className<<"::"<<__func__<<" leave "<<a<<std::endl; \
 }
 
 #define GLOBAL_LOG_ENTER_FUNC(a) { \
-        std::cout<<__func__<<" enter "<<a<<std::endl; \
+        std::cout<<boost::posix_time::microsec_clock::universal_time()<<" "<<__func__<<" enter "<<a<<std::endl; \
 }
 
 #define GLOBAL_LOG_LEAVE_FUNC(a)  { \
-        std::cout<<__func__<<" leave "<<a<<std::endl; \
+        std::cout<<boost::posix_time::microsec_clock::universal_time()<<" "<<__func__<<" leave "<<a<<std::endl; \
 }
 
+#endif //__LOG_HPP__
