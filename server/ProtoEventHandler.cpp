d01a23f774db2dd68b197a742c456515c42a8392
ongonginging <ongonginging@gmail.com>
2015-09-09 07:37:59 +0800

    backup

--- server/ProtoEventHandler.cpp
+++ server/ProtoEventHandler.cpp
@@ -15,12 +15,6 @@ ProtoEventHandler::~ProtoEventHandler(){
     LOG_LEAVE_FUNC("default destructor.");
 }
 
-void ProtoEventHandler::init(){
-    LOG_ENTER_FUNC("");
-    this->base = event_base_new();
-    LOG_LEAVE_FUNC("");
-}
-
 void ProtoEventHandler::setThreadCtrlChanCallback(void (* cb)(evutil_socket_t fd, short event, void* arg), evutil_socket_t fd, void* arg){
     LOG_ENTER_FUNC("");
     this->threadCtrlEvent = event_new(this->base, fd, EV_READ|EV_PERSIST, cb, arg);
@@ -37,6 +31,12 @@ void ProtoEventHandler::setConnCtrlChanCallback(void (* cb)(evutil_socket_t fd, 
 
 void ProtoEventHandler::setReadConnCallback(void (* cb)(evutil_socket_t fd, short event, void* arg), evutil_socket_t fd, void* arg){
     LOG_ENTER_FUNC("");
+    LOG_LEAVE_FUNC("");
+}
+
+void ProtoEventHandler::init(){
+    LOG_ENTER_FUNC("");
+    this->base = event_base_new();
     LOG_LEAVE_FUNC("");
 }
 
